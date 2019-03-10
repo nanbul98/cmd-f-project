a3014c7f484e8a08ef55adf29ee2de2340b08eba
Nandini Bulusu <nanbul785@gmail.com>
2019-03-10 00:17:44 -0800

    Configuring bluetooth

--- app/src/main/java/com/example/mikhaelalayon/appremn/MainActivity.java
+++ app/src/main/java/com/example/mikhaelalayon/appremn/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.mikhaelalayon.appremn;
 
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
+
 public class MainActivity extends AppCompatActivity {
+    private final static int REQUEST_ENABLE_BT = 1;
+
+
+    // Initializes Bluetooth adapter.
+    final BluetoothManager bluetoothManager =
+            (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+    BluetoothAdapter bluetoothAdapter1 = BluetoothAdapter.getDefaultAdapter();
+
+   void bluetoothFunction() {
+       // Ensures Bluetooth is available on the device and it is enabled. If not,
+// displays a dialog requesting user permission to enable Bluetooth.
+       if (bluetoothAdapter1 == null || !bluetoothAdapter1.isEnabled()) {
+           Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+           startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+       }
+   }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
