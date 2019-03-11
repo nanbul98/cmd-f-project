ee6db0141b647b277474c960ce6bc1b60dfd3aa5
Emily Gong <z.gong@alumni.ubc.ca>
2019-03-11 02:24:31 -0700

    adding image detection and stuff

--- app/src/main/java/com/example/mikhaelalayon/appremn/MainActivity.java
+++ app/src/main/java/com/example/mikhaelalayon/appremn/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.mikhaelalayon.appremn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final String TAG = "debug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.take_image);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent intent = new Intent(MainActivity.this, ImageCapture.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
